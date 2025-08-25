500083438182150e37b1d2824097cfc8e5f67f85
abhinav sajeev <abhinavsajeev85@gmail.com>
2025-08-25 15:57:13 +0530

    homepage react

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <footer className='footer py-3 my-3'>
+        <hr className='border-bottom' />
+        <p className='text-light text-center'>&copy; build with 🩶 by Abhinav</p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
