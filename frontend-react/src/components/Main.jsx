924518a412c2fa210eb0b087a89f1ecff6c701ca
abhinav sajeev <abhinavsajeev85@gmail.com>
2025-09-06 17:32:50 +0530

    Registration page

--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Button from './Button'
+import Header from './Header';
+import Footer from "./Footer";
 
 const Main = () => {
   return (
@@ -8,7 +10,7 @@ const Main = () => {
             <div className='p-5 text-center bg-light-dark rounded'>
                 <h1 className='text-light'>Stock Prediction Portal</h1>
                 <p className='text-light lead'>This is a stock prediction application that utilizes advanced machine learning techniques to analyze historical market data, identify trends, and forecast potential future stock prices. The system leverages algorithms such as regression models, time series analysis, and deep learning to provide data-driven insights that can assist investors and traders in making informed financial decisions.</p>
-                <Button text='Login' class='btn btn-outline-info'/>
+                <Button text='Login' className='btn btn-outline-info' url='/login'/>
             </div>
         </div>
     </>
