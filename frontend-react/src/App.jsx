fbf6161a47277b5a50265a8a8123e117e95ad53d
abhinav sajeev <abhinavsajeev85@gmail.com>
2025-09-08 09:25:14 +0530

    login functionality

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -6,12 +6,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Login from './components/Login';
+import AuthProvider from './AuthProvider';
 
 
 function App() {
 
   return (
     <>
+    <AuthProvider>
       <BrowserRouter>
         <Header/>
         <Routes>
@@ -21,6 +23,7 @@ function App() {
         </Routes>
         <Footer/>
       </BrowserRouter>
+    </AuthProvider>
     </>
   )
 }
